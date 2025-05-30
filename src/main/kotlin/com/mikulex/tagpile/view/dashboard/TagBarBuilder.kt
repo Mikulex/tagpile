9df9580157a91b69b6c215598c3f618c06603990
Mikulex <mikulex@mikulex.com>
2025-05-30 22:45:30 +0200

    add button to tag bar for adding a tag to query

--- src/main/kotlin/com/mikulex/tagpile/view/dashboard/TagBarBuilder.kt
+++ src/main/kotlin/com/mikulex/tagpile/view/dashboard/TagBarBuilder.kt
@@ -2,11 +2,14 @@ package com.mikulex.tagpile.view.dashboard
 
 import com.mikulex.tagpile.viewmodel.DashBoardViewModel
 import javafx.event.EventHandler
+import javafx.scene.control.Button
+import javafx.scene.control.ListCell
 import javafx.scene.control.ListView
 import javafx.scene.input.KeyCode
 import javafx.scene.layout.Region
 import javafx.scene.layout.VBox
 import javafx.util.Builder
+import javafx.util.Callback
 import org.slf4j.LoggerFactory
 
 class TagBarBuilder(private val dashboardViewModel: DashBoardViewModel) : Builder<Region> {
@@ -17,15 +20,17 @@ class TagBarBuilder(private val dashboardViewModel: DashBoardViewModel) : Builde
     override fun build(): Region {
         LOG.debug("Initialize TagBar")
         val listView: ListView<String> = ListView(dashboardViewModel.resultTags).apply {
+            cellFactory = Callback { _ -> StringListCell(dashboardViewModel) }
+
             onMouseClicked = EventHandler { event ->
                 if (event.clickCount == 2 && !selectionModel.selectedItem.isNullOrBlank()) {
-                    addToInputHandler()
+                    addToInputHandler(dashboardViewModel)
                 }
             }
 
             onKeyPressed = EventHandler { event ->
                 if (event.code == KeyCode.ENTER) {
-                    addToInputHandler()
+                    addToInputHandler(dashboardViewModel)
                 }
             }
         }
@@ -36,10 +41,31 @@ class TagBarBuilder(private val dashboardViewModel: DashBoardViewModel) : Builde
         }
     }
 
-    private fun ListView<String>.addToInputHandler() {
-        with(dashboardViewModel.searchQuery.get()) {
-            dashboardViewModel.searchQuery.set("$this ${selectionModel.selectedItem}".trim())
-            selectionModel.clearSelection()
+}
+
+private class StringListCell(val model: DashBoardViewModel) : ListCell<String>() {
+    override fun updateItem(item: String?, empty: Boolean) {
+        super.updateItem(item, empty)
+        if (empty || item?.isEmpty() == true) {
+            text = null
+            graphic = null
+        } else {
+            text = item
+            graphic = Button().also { btn ->
+                btn.text = "+"
+                btn.setOnAction {
+                    with(model.searchQuery.get()) {
+                        model.searchQuery.set("$this ${item}".trim())
+                    }
+                }
+            }
         }
+    }
+}
+
+private fun ListView<String>.addToInputHandler(dashboardViewModel: DashBoardViewModel) {
+    with(dashboardViewModel.searchQuery.get()) {
+        dashboardViewModel.searchQuery.set("$this ${selectionModel.selectedItem}".trim())
+        selectionModel.clearSelection()
     }
 }
